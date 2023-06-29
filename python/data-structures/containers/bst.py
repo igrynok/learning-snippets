134c9c37da9cbacf43a660f08a7e581e910a07f9
igrynok <ibragimova.aliya@gmail.com>
2023-06-29 18:13:40 +0200

    BST: first commit with the class Node implementation.

---
+++ python/data-structures/containers/bst.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def find(tree, data):
+    pass
+
+
+def insert(tree, data):
+    pass
