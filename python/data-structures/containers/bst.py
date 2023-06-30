77192ace4531ca991cb4fb395e31e56cd6eed12a
igrynok <ibragimova.aliya@gmail.com>
2023-06-30 12:18:45 +0200

    BST: bug fix, add tests for find and insert functions.

--- python/data-structures/containers/bst.py
+++ python/data-structures/containers/bst.py
@@ -7,31 +7,47 @@ class Node:
 
 def find(tree, data):
     if tree is None:
-        return False
+        print('False')
+        return
     if tree.data == data:
-        return True
-    elif tree.data < data:
-        find(tree.right, data)
-    else:
+        print('Yes')
+        return
+    if tree.data > data:
         find(tree.left, data)
+    else:
+        find(tree.right, data)
 
 
 def insert(tree, data):
-    if tree.data < data and tree.left is None:
+    if tree.data > data and tree.left is None:
         tree.left = Node(data)
         return
-    elif tree.data > data and tree.right is None:
+    elif tree.data < data and tree.right is None:
         tree.right = Node(data)
         return
-    elif tree.data < data:
+    elif tree.data > data:
         insert(tree.left, data)
     else:
         insert(tree.right, data)
 
 
-def remove(tree, data):
-    pass
-
-
 def print_tree(tree):
-    pass
+    if tree:
+        print(tree.data)
+        print_tree(tree.left)
+        print_tree(tree.right)
+
+
+if __name__ == '__main__':
+    root = Node(7)
+    insert(root, 2)
+    insert(root, 11)
+    insert(root, 5)
+    insert(root, 3)
+    insert(root, 6)
+    insert(root, 9)
+    insert(root, 14)
+    insert(root, 13)
+    print_tree(root)
+    find(root, 13)
+    find(root, 33)
