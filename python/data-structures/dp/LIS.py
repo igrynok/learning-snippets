96ad31ff3fbfe14bbd47be76d474cb58a9184a84
igrynok <ibragimova.aliya@gmail.com>
2023-07-10 15:55:51 +0200

    DP:compute the length of the longest subsequence.

---
+++ python/data-structures/dp/LIS.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+# compute the length of longest subsequence
+def longest_sub_len(nums: List[int]) -> int:
+    dp = [0] * (len(nums))
+
+    for i in range(len(nums)):
+        f_max = 0
+        for j in range(i):
+            if nums[j] < nums[i]:
+                f_max = max(dp[j], f_max)
+        dp[i] = f_max + 1
+
+    return max(dp) if dp else 0
+
+
+if __name__ == '__main__':
+    nums = [int(x) for x in input().split()]
+    res = longest_sub_len(nums)
+    print(res)
+    res = longest_sub_len(nums)
+    print(res)
