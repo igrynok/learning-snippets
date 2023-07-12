4a78d83089440e861d02d6f8e8e93325e97479ec
igrynok <ibragimova.aliya@gmail.com>
2023-07-12 11:54:14 +0200

    DP:compute the longest common sequence, refactoring add memoization.

--- python/data-structures/dp/LCS.py
+++ python/data-structures/dp/LCS.py
@@ -1,15 +1,24 @@
 # compute the longest common subsequence of two sequences
 def longest_common_subsequence(word1: str, word2: str) -> int:
-    return count_lcs(word1, word2, len(word1) - 1, len(word2) - 1)
+    memo = {}
+    return count_lcs(word1, word2, memo, len(word1) - 1, len(word2) - 1)
 
 
-def count_lcs(w1, w2, i, j):
+def count_lcs(w1, w2, memo, i, j):
     if i == -1 or j == -1:
         return 0
+
+    if (i, j) in memo:
+        return memo[(i, j)]
+
     if word1[i] == word2[j]:
-        return count_lcs(w1, w2, i - 1, j - 1) + 1
+        total = count_lcs(w1, w2, memo, i - 1, j - 1) + 1
+        memo[(i, j)] = total
+        return total
     else:
-        return max(count_lcs(w1, w2, i, j - 1), count_lcs(w1, w2, i - 1, j))
+        total = max(count_lcs(w1, w2, memo, i, j - 1), count_lcs(w1, w2, memo, i - 1, j))
+        memo[(i, j)] = total
+        return total
 
 
 if __name__ == '__main__':
