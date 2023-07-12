5062c35b223792d8e4a3dea9872ee8d405979ec5
igrynok <ibragimova.aliya@gmail.com>
2023-07-12 11:46:54 +0200

    DP:compute the longest common sequence, refactoring, use indices instead of passing arrays.

--- python/data-structures/dp/LCS.py
+++ python/data-structures/dp/LCS.py
@@ -1,15 +1,15 @@
-
 # compute the longest common subsequence of two sequences
 def longest_common_subsequence(word1: str, word2: str) -> int:
-    def count_lcs(w1, w2):
-        if len(w1) == 0 or len(w2) == 0:
-            return 0
-        if w1[-1] == w2[-1]:
-            return count_lcs(w1[:-1], w2[:-1]) + 1
-        else:
-            return max(count_lcs(w1, w2[:-1]), count_lcs(w1[:-1], w2))
+    return count_lcs(word1, word2, len(word1) - 1, len(word2) - 1)
 
-    return count_lcs(word1, word2)
+
+def count_lcs(w1, w2, i, j):
+    if i == -1 or j == -1:
+        return 0
+    if word1[i] == word2[j]:
+        return count_lcs(w1, w2, i - 1, j - 1) + 1
+    else:
+        return max(count_lcs(w1, w2, i, j - 1), count_lcs(w1, w2, i - 1, j))
 
 
 if __name__ == '__main__':
