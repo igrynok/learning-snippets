7f9e51e13a2dc9b59f34404c32e489edf84b9de9
igrynok <ibragimova.aliya@gmail.com>
2023-09-09 18:31:51 +0200

    Google QA: largest continent with the maximum landlocked area, add is_landlocked and get_neighbours.

--- python/google_qa/landlocked.py
+++ python/google_qa/landlocked.py
@@ -2,11 +2,36 @@ from collections import deque
 
 
 def is_landlocked(cell, grid):
-    pass
+
+    rows = len(grid)
+    columns = len(grid[0])
+
+    if grid[cell[0]][cell[1]] == 1:
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for direction in dirs:
+            if 0 <= cell[0] + direction[0] < rows and 0 <= cell[1] + direction[1] < columns:
+                if grid[cell[0] + direction[0]][cell[1] + direction[1]] == 1:
+                    continue
+                else:
+                    return False
+        return True
+    else:
+        return False
 
 
 def get_neighbours(current, grid, landlocked):
-    pass
+
+    rows = len(grid)
+    columns = len(grid[0])
+
+    neighbours = []
+    dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    for direction in dirs:
+        if 0 <= current[0] + direction[0] < rows and 0 <= current[1] + direction[1] < columns:
+            if grid[current[0] + direction[0]][current[1] + direction[1]] == 1:
+                neighbours.append((current[0] + direction[0], current[1] + direction[1]))
+
+    return neighbours
 
 
 def bfs(cell, landlocked, visited, grid):
